b764ebbfa8497370a3be6bc06d967a61bfa84a32
Pablo Marin <pablomarin647@gmail.com>
2026-04-14 15:13:49 -0500

    fix navegación dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Dashboard() {
   return (
     <div className="container mt-5 text-center">
@@ -7,21 +9,27 @@ export default function Dashboard() {
         <div className="col-md-4">
           <div className="card shadow p-3">
             <h5>Vehículos</h5>
-            <a href="/vehiculos" className="btn btn-dark">Ir</a>
+            <Link to="/vehiculos" className="btn btn-dark">
+              Ir
+            </Link>
           </div>
         </div>
 
         <div className="col-md-4">
           <div className="card shadow p-3">
             <h5>Usuarios</h5>
-            <a href="/usuarios" className="btn btn-dark">Ir</a>
+            <Link to="/usuarios" className="btn btn-dark">
+              Ir
+            </Link>
           </div>
         </div>
 
         <div className="col-md-4">
           <div className="card shadow p-3">
             <h5>Reservas</h5>
-            <a href="/reservas" className="btn btn-dark">Ir</a>
+            <Link to="/reservas" className="btn btn-dark">
+              Ir
+            </Link>
           </div>
         </div>
       </div>
