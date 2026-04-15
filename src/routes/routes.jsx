932446eae4a3b1f56d69bf01317957470607c5c1
Pablo Marin <pablomarin647@gmail.com>
2026-04-14 20:36:13 -0500

    fix final stable app

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,8 +4,8 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
-import Usuario from "../pages/Usuarios";
-import Vehiculo from "../pages/Vehiculos";
+import Usuarios from "../pages/Usuarios";
+import Vehiculos from "../pages/Vehiculos";
 import Reservas from "../pages/Reservas";
 
 export default function AppRoutes() {
@@ -15,8 +15,8 @@ export default function AppRoutes() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/usuarios" element={<Usuario />} />
-      <Route path="/vehiculos" element={<Vehiculo />} />
+      <Route path="/usuarios" element={<Usuarios />} />
+      <Route path="/vehiculos" element={<Vehiculos />} />
       <Route path="/reservas" element={<Reservas />} />
     </Routes>
   );
