c698d5d55754100b764064f483976057e991dc4d
Pablo Marin <pablomarin647@gmail.com>
2026-04-14 20:10:05 -0500

    fix routes export default

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,21 @@
 import Usuario from "../pages/Usuarios";
 import Vehiculos from "../pages/Vehiculos";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import Reservas from "../pages/Reservas";
 
-export { Usuario, Vehiculos };
+import { Routes, Route } from "react-router-dom";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/usuarios" element={<Usuario />} />
+      <Route path="/vehiculos" element={<Vehiculos />} />
+      <Route path="/reservas" element={<Reservas />} />
+    </Routes>
+  );
+}
